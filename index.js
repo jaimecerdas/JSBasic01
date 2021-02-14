af11f73495fd1998f5478770205754b980dc2d23
J Cerdas <jaimecerdas@gmail.com>
2021-02-14 01:36:22 +0000

    Excuse generator v1 by Jaime

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 
-let nombre;
-nombre = "jaime";
+let whoNumber = Math.floor(Math.random() * 4); 
+console.log(whoNumber)
+let whatNumber = Math.floor(Math.random() * 4); 
+console.log(whatNumber)
+let whenNumber = Math.floor(Math.random() * 5); 
+console.log(whenNumber)
 
-console.log('Jaime Cerdas')
+let who = ['the dog','my granma','his turtle','my bird'];
+let what = ['eat','pissed','crushed','broked'];
+let when = ['before the class','right in time','when I finished','during my lunch','while I was praying'];
+
+document.getElementById("excuse").innerHTML = who[whoNumber]+' '+what[whatNumber]+' '+when[whenNumber];
